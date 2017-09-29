3405a4134f3e512570437b5b8678cfaf0d7ffa69
John Furtado <jf8846@gmail.com>
2017-09-29 00:04:50 +0000

    custom directives and templates using angular

---
+++ app.js
@@ -0,0 +1,69 @@
+(function() {
+    var app = angular.module('store', []);
+    // controller for the page
+    app.controller('StoreController', function() {
+        this.products = gems;    
+    });
+    // controller for the panels
+    app.controller('PanelController', function() {
+        this.tab = 1;
+        
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab){
+            return this.tab === checkTab;
+        };
+    });
+    //controller for review form
+    app.controller('ReviewController', function() {
+        this.review = {};
+        
+        this.addReview = function(product) {
+            this.review.createdOn = Date.now(); 
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+    //custom directive, product-title in html productTitle in js
+    app.directive('productTitle', function(){
+       return {
+        //type of directive E for html element    
+        restrict: 'E',
+        //template URL
+        templateUrl: 'product-title.html'
+       }; 
+    });
+    // Gems as an array of objects
+    var gems = [{
+        name: 'Dodecahedron',
+        price: 2,
+        description: 'Fancy',
+        canPurchase: true,
+        soldOut: false,
+        reviews: [{
+            stars: 5,
+            body: "Loveee ittt!",
+            author: "very@excited.com"
+        },{
+            stars: 1,
+            body: "Nope",
+            author: "very@disappointed.com"            
+        }]
+    },{
+        name: 'Pentagonal Gem',
+        price: 5.95,
+        description: 'Five sides',
+        canPurchase: true,
+        soldOut: false,
+        reviews: [{
+            stars: 4,
+            body: "Liked it",
+            author: "kinda@excited.com"
+        },{
+            stars: 3,
+            body: "Ok",
+            author: "not@disappointed.com"            
+        }]        
+    }];
+})();    
